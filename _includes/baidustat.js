fb9abe138bfecb788eb3fc24b79ba2e43bd47984
seamaner <quhxquhx@gmail.com>
2022-04-26 18:54:21 +0800

    Delete _includes directory

--- _includes/baidustat.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?61880728ede2e852a3b0ecaa1bec4644";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
